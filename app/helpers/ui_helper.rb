f721946fcb460e7d1fa91481bd46579e3ff7e2d0
Pavel Belyakov <me@puffy.name>
2014-06-06 22:29:41 +0400

    Add more helpers
    from https://gist.github.com/camerond/4637757

--- app/helpers/ui_helper.rb
+++ app/helpers/ui_helper.rb
@@ -3,6 +3,11 @@ module UiHelper
     range.to_a.sample
   end
 
+  def lorem(paras)
+    paras = around(paras) if paras.is_a?(Range)
+    Faker::Lorem.paragraphs(paras).map{|p| "<p>#{p}</p>"}.join.html_safe
+  end
+
   def lorem_words(num)
     num = around(num) if num.is_a?(Range)
     Faker::Lorem.words(num).join(' ')
@@ -10,5 +15,9 @@ module UiHelper
 
   def coinflip
     around([true, false])
+  end
+
+  def sometimes(num = 3)
+    yield if num == around(1..num)
   end
 end
