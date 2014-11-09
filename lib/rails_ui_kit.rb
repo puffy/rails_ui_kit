af90912f7566bbe8204bb587d379f21f17264a4a
Pavel Belyakov <me@puffy.name>
2014-11-09 11:57:47 +0300

    Use ffaker gem if it exist in bundle
    to avoid collision with faker gem.
    
    (ffaker gem, for example, used in spree)

--- lib/rails_ui_kit.rb
+++ lib/rails_ui_kit.rb
@@ -1,5 +1,10 @@
 require 'rails_ui_kit/engine'
-require 'faker'
+
+if Gem::Specification.find_all_by_name('ffaker').any?
+  require 'ffaker' unless defined?(Faker)
+else
+  require 'faker' unless defined?(Faker)
+end
 
 module RailsUiKit
 end
